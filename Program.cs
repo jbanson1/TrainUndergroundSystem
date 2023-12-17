41b89c86e3acec7d3a7eaf5fba92969eea923119
Joseph Banson <33466255+jbanson1@users.noreply.github.com>
2023-12-17 19:14:15 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TrainUndergroundSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
